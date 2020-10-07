e8ab6cf731717743e403dbb371abac3dad974274
Saranya Kannan <saranyakannan.v@gmail.com>
2020-10-07 15:57:09 +0200

    variables

---
+++ calcvariables.js
@@ -0,0 +1,8 @@
+var x = 10;
+var y = 2;
+var a=x+y;
+var b=x/y;
+var c=x%y;
+document.getElementById("sum").innerHTML = a;
+document.getElementById("mul").innerHTML = b;
+document.getElementById("mod").innerHTML = c;
